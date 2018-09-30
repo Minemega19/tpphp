fee93bfa6be84f167b89f9688eefa79f58185896
Minemega19 <themega19@gmail.com>
2018-09-30 21:20:17 +0200

    It works

--- ex2p3.php
+++ ex2p3.php
@@ -1,13 +1,43 @@
-<html>
+<?php
+  //require_once("include/config.inc.php");
+  //require_once("include/connect.inc.php");
+?>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-<meta http-equiv="Content-type" content="text/html;charset=iso-8859-1">
-<link rel="stylesheet" type="text/css" href="mycss.css">
-<title>MERCI de remplir le formulaire</title>
+  <meta charset="utf-8">
+  <title>TP2 PHP</title>
 </head>
 <body>
+  <table border="3">
+    <tr>
+      <th>Nom</th>
+      <th>Prénom</th>
+      <th>Mail</th>
+      <th>Inscrit</th>
+    </tr>
+
     <?php
-        $sql = "INSERT INTO CLIENT(NOM_CLIENT, PRENOM_CLIENT, MAIL_CLIENT, INSCRIT_CLIENT) VALUES ('$nom', '$prenom','$email','$inscrit')";
-        $send = mysqli_query($db,$sql);
+      $db = mysqli_connect ("localhost","root","");
+        mysqli_select_db($db,"client"); 
+      $sql = "SELECT NOM_CLIENT, PRENOM_CLIENT, MAIL_CLIENT, INSCRIT_CLIENT FROM CLIENT";
+      $result = mysqli_query($db,$sql);
+
+      while($ligne = mysqli_fetch_array($result)){
     ?>
-    </body>
-    
+        <tr>
+          <td><?php echo $ligne["NOM_CLIENT"] ?></td>
+          <td><?php echo $ligne["PRENOM_CLIENT"] ?></td>
+          <td><?php echo $ligne["MAIL_CLIENT"] ?></td>
+          <?php
+          if($ligne['INSCRIT_CLIENT'] == 0){
+            ?>
+            <td>NON</td> </tr>
+            <?php
+          }else{
+            ?>
+            <td>OUI</td> </tr>
+            <?php
+          }
+      }
+    ?>
